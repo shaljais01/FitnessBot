a5440c8ad42f121583bb928766d6c5c1786fe833
mansigoel98 <goel.mansi98@gmail.com>
2019-10-01 09:41:55 +0530

    hk

--- bot.js
+++ bot.js
@@ -8,7 +8,24 @@ class EchoBot extends ActivityHandler {
         super();
         // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
         this.onMessage(async (context, next) => {
-            await context.sendActivity(`You said '${ context.activity.text }'`);
+            const https = require('https');
+
+https.get('https://api.nasa.gov/planetary/apod?api_key=context', (resp) => {
+  let data = '';
+
+  // A chunk of data has been recieved.
+  resp.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  // The whole response has been received. Print out the result.
+  resp.on('end', () => {
+    await context.sendActivity(JSON.parse(data).explanation);
+  });
+
+}).on("error", (err) => {
+    await context.sendActivity("Error: " + err.message);
+});
 
             // By calling next() you ensure that the next BotHandler is run.
             await next();
